a4a136148d2d716c5096f93cb42b488d317d822c
Leonardo <leonardo.guilherme@gmail.com>
2016-07-20 14:58:35 -0300

    Updating doc

--- doc/search/functions_1.js
+++ doc/search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['direction',['direction',['../classtweeny_1_1tween_a69fe160d5a57e60c8be43671e4fda494.html#a69fe160d5a57e60c8be43671e4fda494',1,'tweeny::tween::direction()'],['../classtweeny_1_1tween_3_01T_01_4_a69fe160d5a57e60c8be43671e4fda494.html#a69fe160d5a57e60c8be43671e4fda494',1,'tweeny::tween&lt; T &gt;::direction()']]],
   ['duration',['duration',['../classtweeny_1_1tween_a996f2f6c960641738fc46d3f39f6ef6f.html#a996f2f6c960641738fc46d3f39f6ef6f',1,'tweeny::tween::duration()'],['../classtweeny_1_1tween_3_01T_01_4_a996f2f6c960641738fc46d3f39f6ef6f.html#a996f2f6c960641738fc46d3f39f6ef6f',1,'tweeny::tween&lt; T &gt;::duration()']]],
   ['during',['during',['../classtweeny_1_1tween_aa2a9ee1dd932737bb4e282a428512fd0.html#aa2a9ee1dd932737bb4e282a428512fd0',1,'tweeny::tween::during()'],['../classtweeny_1_1tween_3_01T_01_4_aa0e033fe7b270676bcbfa3bfffe7e73c.html#aa0e033fe7b270676bcbfa3bfffe7e73c',1,'tweeny::tween&lt; T &gt;::during()']]]
 ];
