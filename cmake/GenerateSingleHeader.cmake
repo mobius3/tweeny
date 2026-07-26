1e8f2335462377d5af503fb665c476ecc7bcd6ee
Leonardo <leonardo.guilherme@gmail.com>
2026-07-26 07:59:34 -0300

    use quom directly from uvx

--- cmake/GenerateSingleHeader.cmake
+++ cmake/GenerateSingleHeader.cmake
@@ -1,22 +1,12 @@
 # This cmake script is used to generate a single header file with all of tweeny
-find_package(Python3 3.6 COMPONENTS Interpreter QUIET)
-
-if (NOT Python3_Interpreter_FOUND)
-  message(STATUS "Python 3.6+ interpreter not found. Single-header include file will NOT be created")
-  return()
-endif()
-
-find_program(QUOM_EXECUTABLE NAMES quom)
-if (NOT QUOM_EXECUTABLE)
-  message(STATUS "quom program not found. Install it with: pip3 install quom")
-  return()
-endif()
+find_program(UVX_EXECUTABLE NAMES uvx REQUIRED)
 
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/single-header)
 
 add_custom_target(single-header
-  COMMAND
-  ${QUOM_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/include/tweeny.h ${CMAKE_CURRENT_BINARY_DIR}/single-header/tweeny-${Tweeny_VERSION}.h
+  COMMAND ${UVX_EXECUTABLE} quom
+    ${CMAKE_CURRENT_SOURCE_DIR}/include/tweeny/tweeny.h
+    ${CMAKE_CURRENT_BINARY_DIR}/single-header/tweeny-${Tweeny_VERSION}.h
   WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
   COMMENT "Generating single header file"
 )
