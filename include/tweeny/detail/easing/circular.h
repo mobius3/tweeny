24abd640c97c9a8c3b14a56c477f8ec6a434a95a
Leonardo <leonardo.guilherme@gmail.com>
2026-07-26 07:59:20 -0300

    update copyright notice

--- include/tweeny/detail/easing/circular.h
+++ include/tweeny/detail/easing/circular.h
@@ -1,7 +1,7 @@
 /*
 This file is part of the Tweeny library.
 
-Copyright (c) 2016-2025 Leonardo Guilherme Lucena de Freitas
+Copyright (c) 2016-2026 Leonardo Guilherme Lucena de Freitas
 Copyright (c) 2016 Guilherme R. Costa
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -10,7 +10,7 @@ the Software without restriction, including without limitation the rights to
 use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
 the Software, and to permit persons to whom the Software is furnished to do so,
 subject to the following conditions:
-#
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
