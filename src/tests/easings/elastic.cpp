bee02199c2a3118a1f4d5721fa1ac3acc630e8e2
Leonardo <leonardo.guilherme@gmail.com>
2026-08-02 23:06:05 -0300

    add elastic easing unit tests

---
+++ src/tests/easings/elastic.cpp
@@ -0,0 +1,16 @@
+#include <catch2/catch_test_macros.hpp>
+#include <tweeny/easing.h>
+
+#include "test-easing.h"
+
+TEST_CASE("easing::elasticIn matches Penner easeInElastic samples", "[easing][elastic]") {
+  test_easing(tweeny::easing::elasticIn, easeInElastic);
+}
+
+TEST_CASE("easing::elasticOut matches Penner easeOutElastic samples", "[easing][elastic]") {
+  test_easing(tweeny::easing::elasticOut, easeOutElastic);
+}
+
+TEST_CASE("easing::elasticInOut matches Penner easeInOutElastic samples", "[easing][elastic]") {
+  test_easing(tweeny::easing::elasticInOut, easeInOutElastic);
+}
